40a03627dd41108369ba68daed04f3a13d57d1e1
MehmetZiya <mehmetziya82@gmail.com>
2024-09-19 16:09:05 +0200

    Toolbar component created

--- src/app/workspaces/[workspaceId]/page.tsx
+++ src/app/workspaces/[workspaceId]/page.tsx
@@ -1,11 +1,12 @@
-interface WorkspaceIdPageParams {
-  params: {
-    workspaceId: string
-  }
-}
+'use client'
 
-const WorkspaceIdPage = ({ params }: WorkspaceIdPageParams) => {
-  return <div>ID: {params.workspaceId} </div>
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace'
+import { useWorkspaceId } from '@/hooks/use-worspace-id'
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId()
+  const { data } = useGetWorkspace({ id: workspaceId })
+  return <div> Data : {JSON.stringify(data)} </div>
 }
 
 export default WorkspaceIdPage
